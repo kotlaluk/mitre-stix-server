4fa8da14d90f16fe385a6ebffdabe625a74d8aaf
Lukas Kotlaba <lukas.kotlaba@gmail.com>
2020-12-28 20:41:30 +0100

    Create basic server structure

---
+++ build.sbt
@@ -0,0 +1,13 @@
+name := "mitre-stix-server"
+
+version := "0.1"
+
+scalaVersion := "2.13.4"
+
+idePackagePrefix := Some("org.example.mitrestixserver")
+
+libraryDependencies ++= Seq(
+  "org.http4s" %% "http4s-dsl" % "0.21.14",
+  "org.http4s" %% "http4s-blaze-server" % "0.21.14",
+  "org.http4s" %% "http4s-blaze-client" % "0.21.14"
+)
---
+++ src/main/scala/Server.scala
@@ -0,0 +1,26 @@
+package org.example.mitrestixserver
+
+import cats.effect._
+import org.http4s.HttpRoutes
+import org.http4s.dsl.io._
+import org.http4s.implicits._
+import org.http4s.server.blaze._
+import scala.concurrent.ExecutionContext.global
+
+
+object Server extends IOApp {
+
+  val helloWorldService = HttpRoutes.of[IO] {
+    case GET -> Root / "hello" =>
+      Ok(s"Hello, world!")
+  }.orNotFound
+
+  def run(args: List[String]): IO[ExitCode] =
+    BlazeServerBuilder[IO](global)
+      .bindHttp(8080, "localhost")
+      .withHttpApp(helloWorldService)
+      .serve
+      .compile
+      .drain
+      .as(ExitCode.Success)
+}
