208f90858e2d5bff88f484d0fbe70db66124a734
Lukas Kotlaba <lukas.kotlaba@gmail.com>
2020-12-29 12:52:06 +0100

    Add JSON and logging support

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -3,16 +3,21 @@ package org.example.mitrestixserver
 import cats.effect._
 import org.http4s.HttpRoutes
 import org.http4s.dsl.io._
-import org.http4s.implicits._
 import org.http4s.server.blaze._
-import scala.concurrent.ExecutionContext.global
 
+import scala.concurrent.ExecutionContext.global
+import io.circe.syntax._
+import io.circe.generic.auto._
+import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
+import org.http4s.implicits.http4sKleisliResponseSyntaxOptionT
+
+private case class Hello(name: String)
 
 object Server extends IOApp {
 
   val helloWorldService = HttpRoutes.of[IO] {
     case GET -> Root / "hello" =>
-      Ok(s"Hello, world!")
+      Ok(Hello("world").asJson)
   }.orNotFound
 
   def run(args: List[String]): IO[ExitCode] =
